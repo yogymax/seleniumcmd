6fbec0057bd54d30fb1519563966c6123bdd19f1
Sangeetha <Sangeetha@Sang>
2017-03-18 22:17:39 +0530

    Committed windws handler

---
+++ src/test/java/frames/Practice/FramesSample.java
@@ -0,0 +1,32 @@
+package frames.Practice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FramesSample {
+	
+
+	public static void main(String[] args) {
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://demo.guru99.com/selenium/guru99home/");
+		driver.manage().window().maximize();
+		int size = driver.findElements(By.tagName("iframe")).size();
+		System.out.println(size);
+	   List<WebElement> web= driver.findElements(By.tagName("iframe"));
+	   for(WebElement w: web)
+	   {
+		   String att=w.getAttribute("id");
+		   driver.switchTo().frame(att);
+		   System.out.println(att);
+	   }
+	   
+		
+		driver.close();
+
+	}
+
+}
---
+++ src/test/java/frames/Practice/WindowHandler.java
@@ -0,0 +1,32 @@
+package frames.Practice;
+
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WindowHandler {
+
+	public static void main(String[] args) {
+		WebDriver driver= new FirefoxDriver();
+		
+		driver.get("http://toolsqa.wpengine.com/automation-practice-switch-windows/");      
+
+        String handle= driver.getWindowHandle();
+        System.out.println("windowhandle"+handle); 
+      //  WebDriverWait wait = new WebDriverWait(driver, 5);
+		//wait.until(ExpectedConditions.presenceOfElementLocated());
+       // driver.findElement(By.name("New Message Window")).click();
+        Set<String> handles = driver.getWindowHandles();
+        System.out.println(handles);
+        for (String handle1 : handles)
+        {
+        	System.out.println(handle1);
+        	driver.switchTo().window(handle1);
+        }     
+        driver.switchTo().window(handle); //switchig back to parent
+        driver.close();        
+
+	}
+
+}
